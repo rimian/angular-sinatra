1cf32722dde3847f24489355a7364c25c40b6fb9
Rimian Perkins <hello@rimian.com.au>
2015-07-02 16:00:08 +1000

    demo controller

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 (function() {
   'use strict';
 
+  angular.module('myApp', [
+    'ngRoute',
+    'myApp.demo'
+  ])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.otherwise({redirectTo: '/demo'});
+  }]);
+
+  angular.module('myApp.demo', ['ngRoute'])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/demo', {
+      templateUrl: 'partials/demo.html',
+      controller: 'DemoCtrl'
+    });
+  }])
+
+  .controller('DemoCtrl', [function() {
+
+  }]);
+
 
 })();
--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,12 +1,15 @@
 
 feature 'home page yo' do
   before do
-    app = Sinatra.new
+    app = Sinatra.new do
+
+    end
     Capybara.app = App.new(app)
   end
 
   scenario 'Visit home page and something is there' do
     visit '/'
     expect(page).to have_content 'Angular Sinatra'
+    expect(page).to have_content 'Demo'
   end
 end
